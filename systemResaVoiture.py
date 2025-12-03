a2f36d02513ec665cb66e9159776d97093da28a7
Theboubou <theboubou49@gmail.com>
2025-12-03 11:37:42 +0100

    cdqvcqdc

---
+++ systemResaVoiture.py
@@ -0,0 +1,14 @@
+import vehicule
+import client
+import reservation
+
+class SystemResaVehicule:
+    vehicules = []
+    voitures = []
+    camions = []
+    motos = []
+    reservations = []
+    clients = []
+
+    def generRapport():
+        for elt in: 
