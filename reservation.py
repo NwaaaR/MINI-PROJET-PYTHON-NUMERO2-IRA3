31193425946d2f77b6135c2b892b8064d9566935
Theboubou <theboubou49@gmail.com>
2025-12-10 09:46:20 +0100

    vdniunvidjs

---
+++ reservation.py
@@ -0,0 +1,10 @@
+
+class Reservation(): 
+    def __init__(self, client, vehicule, date, cout_total):
+        ## Location via la classe véhicule
+        self.client = client
+        self.vehicule = vehicule
+        self.date = date
+        self.cout_total = cout_total
+
+        
--- systemResaVoiture.py
+++ systemResaVoiture.py
@@ -10,5 +10,22 @@ class SystemResaVehicule:
     reservations = []
     clients = []
 
-    def generRapport():
-        for elt in: 
+    def reserver():
+        reservation = 
+
+    def generRapportVehiculeDispo():
+        vehiculeDispo = []
+        for elt in SystemResaVehicule.vehicules:
+            if elt.id not in SystemResaVehicule.reservationsc :
+                vehiculeDispo.append(elt)
+
+    def generRapportLoc():
+        return SystemResaVehicule.reservations     
+    
+    def generRapportCA():
+        tot = 0 
+        for elt in SystemResaVehicule.reservations:
+            tot += elt.cout_total 
+        return tot    
+
+
